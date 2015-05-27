d5d1059985f7f8e4bc75ca9409ef691ec9dace36
Sooham Rafiz <rafizsooham@gmail.com>
2015-05-27 00:22:54 -0400

    Added ability for boss multiplication and further PythonSetup

--- Assets/Scripts/PythonSetup.py
+++ Assets/Scripts/PythonSetup.py
@@ -2,24 +2,32 @@ import os
 import re
 
 
-def correct_bash_profile_applescript_path():
+def correct_bash_profile_paths():
     ''' NoneType -> NoneType
 
         Allows this python script daemon to modify and correct the
-        path for applescript file CloseFinderWindows.applescript in
-        .bash_profile (in the same directory).
+        path for applescript file CloseFinderWindows.applescript
+        and BossMultiply.bash in .bash_profile (in the same directory).
     '''
-    applescript_run_cmd = re.compile('osascript.*;\s')
+    applescript_run_cmd = re.compile(r'osascript.*;\s*builtin')
+    bash_run_cmd = re.compile(r'bash.*;\s*fi')
 
     with open('.bash_profile') as fin:
         contents = fin.read()
 
-    new_command = (
-        'osascript ' + os.path.abspath('CloseFinderWindows.applescript') + '; '
+    new_command_aplscpt = (
+        'osascript ' + os.path.abspath('CloseFinderWindows.applescript') +
+        '; builtin'
         )
-    contents = applescript_run_cmd.sub(new_command, contents)
+
+    new_command_bash = (
+        'bash ' + os.path.abspath('BossMultiply.bash') +
+        '; fi'
+        )
+    contents = applescript_run_cmd.sub(new_command_aplscpt, contents)
+    contents = bash_run_cmd.sub(new_command_bash, contents)
 
     with open('.bash_profile', 'w') as fout:
         fout.write(contents)
 
-correct_bash_profile_applescript_path()
+correct_bash_profile_paths()
