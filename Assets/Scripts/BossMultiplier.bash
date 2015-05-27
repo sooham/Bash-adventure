75ba12898a2e41d8fec010d7c2e7c1ac1d62dd23
Sooham Rafiz <rafizsooham@gmail.com>
2015-05-27 00:45:27 -0400

    boss quine multiplication bugfix; it finally works!'

--- Assets/Scripts/BossMultiplier.bash
+++ Assets/Scripts/BossMultiplier.bash
@@ -1,13 +1,8 @@
 #!/bin/bash
 HAMSTER=2
 while [ $HAMSTER -le 100 ]; do
-        if [ ! -d ../../Game/Door/Door/Door_2/Door/Door/Door_1/Door/Door/Door/.Door/boss_door ]; then 
-            # quit the loop
-            break
-        fi
-
         # copy the hamster file with adequate numbering 
-        cp ../../Game/Door/Door/Door_2/Door/Door/Door_1/Door/Door/Door/.Door/boss_door/boss ../../Game/Door/Door/Door_2/Door/Door/Door_1/Door/Door/Door/.Door/boss_door/boss_$HAMSTER
+        cp ./boss ./boss_$HAMSTER
         let HAMSTER=HAMSTER+1
         sleep 1 # replicate every 4 seconds
 done
