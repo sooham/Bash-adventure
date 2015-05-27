f4da99766f22d9a1a55dfd156569de9274faf3b8
Sooham Rafiz <sooham@users.noreply.github.com>
2015-05-27 01:44:02 -0400

    Fixed comment mistake

--- Assets/Scripts/BossMultiplier.bash
+++ Assets/Scripts/BossMultiplier.bash
@@ -4,5 +4,5 @@ while [ $HAMSTER -le 100 ]; do
         # copy the hamster file with adequate numbering 
         cp ./boss ./boss_$HAMSTER
         let HAMSTER=HAMSTER+1
-        sleep 1 # replicate every 4 seconds
+        sleep 1 # replicate every second
 done
